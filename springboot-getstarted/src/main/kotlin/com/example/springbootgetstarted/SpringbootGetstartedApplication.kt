dbe3f8609f3e6c70be6480cbfb6ed4567090c2e2
ishizakamasaki <robomasaki2@gmail.com>
2020-11-03 08:10:34 +0900

    first commit

---
+++ springboot-getstarted/src/main/kotlin/com/example/springbootgetstarted/SpringbootGetstartedApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootgetstarted
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootGetstartedApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootGetstartedApplication>(*args)
+}
---
+++ springboot-getstarted/src/test/kotlin/com/example/springbootgetstarted/SpringbootGetstartedApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springbootgetstarted
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootGetstartedApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
