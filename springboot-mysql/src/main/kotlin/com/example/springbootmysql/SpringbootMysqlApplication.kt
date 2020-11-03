995cf964d5414242c108928bfeec86de5b06e142
ishizakamasaki <robomasaki2@gmail.com>
2020-11-03 09:11:10 +0900

    first commit

---
+++ springboot-mysql/src/main/kotlin/com/example/springbootmysql/SpringbootMysqlApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootmysql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootMysqlApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootMysqlApplication>(*args)
+}
