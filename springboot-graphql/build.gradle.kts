2591c0bada867952174f82e44bc68414a13d9597
masaki <robomasaki2@gmail.com>
2020-11-04 17:15:29 +0900

    added graphql dependency

--- springboot-graphql/build.gradle.kts
+++ springboot-graphql/build.gradle.kts
@@ -16,13 +16,20 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter")
+
+    implementation ("com.graphql-java:graphql-spring-boot-starter:5.0.2")
+    implementation ("com.graphql-java:graphiql-spring-boot-starter:5.0.2")
+    implementation ("com.graphql-java:voyager-spring-boot-starter:5.0.2")
+    implementation ("com.graphql-java:graphql-java-tools:5.2.4")
 }
 
 tasks.withType<Test> {
