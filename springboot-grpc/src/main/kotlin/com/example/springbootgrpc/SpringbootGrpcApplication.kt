d511cc0998c508cec76ec28b2724f24eb6ecc5e3
masaki <robomasaki2@gmail.com>
2020-11-04 17:37:33 +0900

    first commit

---
+++ springboot-grpc/src/main/kotlin/com/example/springbootgrpc/SpringbootGrpcApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootgrpc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootGrpcApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootGrpcApplication>(*args)
+}
