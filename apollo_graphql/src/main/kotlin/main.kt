283a0d1738d672cf0642d446276d4d7e30ea3515
masaki <robomasaki2@gmail.com>
2020-11-25 16:14:31 +0900

    feat: added mutation

--- apollo_graphql/src/main/kotlin/main.kt
+++ apollo_graphql/src/main/kotlin/main.kt
@@ -1,4 +1,5 @@
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Input
 import com.apollographql.apollo.coroutines.toDeferred
 import kotlinx.coroutines.runBlocking
 
@@ -7,12 +8,16 @@ fun main() = runBlocking {
         .serverUrl("https://apollo-fullstack-tutorial.herokuapp.com")
         .build()
 
-    // シンプルなクエリ
+    // シンプルなQuery
     val responseList = apolloClient.query(LaunchListQuery()).toDeferred().await()
     println(responseList.data?.launches)
 
-    // パラメータを使ったクエリ(idを指定する)
+    // パラメータを使ったQuery(idを指定する)
     val launch = responseList.data?.launches?.launches?.first() ?: return@runBlocking
     val responseDetail = apolloClient.query(LaunchDetailsQuery(id = launch.id)).toDeferred().await()
     println(responseDetail.data?.launch)
+
+    // Mutation
+    val responseLogin = apolloClient.mutate(LoginMutation(email = Input.fromNullable("example@example.com"))).toDeferred().await()
+    println(responseLogin.data?.login)
 }
